359436b94dcb37c3ce51300a9b00d33bb4ec9326
smallgeek <smallgeek@outlook.jp>
2024-11-13 13:52:33 +0900

    Added a program for monitoring SessionSwitch events

---
+++ SessionSwitchTest/SessionSwitchTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
+using NLog.Extensions.Logging;
+
+namespace SessionSwitchTest
+{
+    internal class Program
+    {
+        static AutoResetEvent autoEvent = new AutoResetEvent(false);
+        static ILogger<Program> logger;
+
+        static void Main(string[] args)
+        {
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddNLog());
+            logger = loggerFactory.CreateLogger<Program>();
+
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+
+            autoEvent.WaitOne();
+        }
+
+        private static void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            logger.LogDebug("{SessionSwitch}", e.Reason);
+        }
+    }
+}
